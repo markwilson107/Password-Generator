37be99e6e73e8e82b1037c39cb52d1fa1b1de0c4
Mark Wilson <markwilson107@hotmail.com>
2020-09-11 20:49:50 +0800

    Improved character type prompt.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Assignment Code
+//Variables
 var generateBtn = document.querySelector("#generate");
 var passLength = "";
 var charType = "";
-var charTypeArray = ["lowercase", "uppercase", "numeric", "special"];
+var charTypeArray = ["l", "u", "n", "s"];
 var charSet = "";
 var passwordValue = "";
 
@@ -11,15 +12,16 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
+
 }
 
 //Generate Password
 function generatePassword() {
-  
+
   //prompt user for password length
   passLength = prompt("What length should the password be? (8-128)");
   //repeat prompt until correct criteria is met or cancel is pressed
-  while (passLength != null && (passLength < 8 || passLength > 128)) {
+  while (passLength != null && passLength < 8 || passLength > 128) {
     //prompt user for password length
     passLength = prompt("What length should the password be? (8-128)\nPlease enter valid length. ");
   }
@@ -27,8 +29,9 @@ function generatePassword() {
   if (passLength === null) {
     return "";
   }
+
   //prompt user for character type(s)
-  var charType = prompt("What character types would you like? (lowercase, uppercase, numeric, and/or special)\nList as many as you would like seporated by a space.");
+  charType = prompt("What character types would you like?\nlowercase(l), uppercase(u), numeric(n), and/or special(s)\nList as many letters as you like.");
   //converts string to lowercase
   if (charType != null) {
     charType = charType.toLowerCase();
@@ -36,13 +39,13 @@ function generatePassword() {
   //repeat prompt until correct criteria is met or cancel is pressed
   while (charType != null && !charTypeArray.some(v => charType.includes(v))) {
     //prompt user for character type(s)
-    charType = prompt("What character types would you like? (lowercase, uppercase, numeric, and/or special)\nList as many as you like.\nPlease enter valid character type(s).");
+    charType = prompt("What character types would you like?\nlowercase(l), uppercase(u), numeric(n), and/or special(s)\nPlease enter valid character types.");
     //converts string to lowercase
     if (charType != null) {
       charType = charType.toLowerCase();
     }
   }
-  //if user presses cancel end fucntion
+  //if user presses cancel end function
   if (charType === null) {
     return "";
   }
@@ -74,11 +77,12 @@ function generatePassword() {
   //generate password
   //clear any previous value
   passwordValue = "";
-  //loops through length 
+  //loops through password length 
   for (var i = 0, n = charSet.length; i < passLength; ++i) {
-    //adds random characters
+    //adds random characters from charSet
     passwordValue += charSet.charAt(Math.floor(Math.random() * n));
   }
+  //returns the final password
   return passwordValue;
 }
 
